6ff808e1b8551dc862451eb13f21bea3b4780f4b
Markus Holopainen <markusp_holopainen@yahoo.com>
2022-07-27 15:31:15 +0300

    5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,34 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [compact, setCompact] = useState(true)
+
+  const blogStyle = {
+    paddingTop: 6,
+    paddingBottom: 6,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+    cursor: 'pointer',
+  }
+  return (
+    <div style={blogStyle}>
+      <div onClick={() => setCompact(!compact)}>
+        {blog.title} {blog.author} <button>{compact ? 'view' : 'hide'}</button>
+      </div>
+      {!compact && (
+        <>
+          <div>{blog.url}</div>
+          <div>
+            likes: {blog.likes}{' '}
+            <button onClick={(event) => event.preventDefault()}>like</button>
+          </div>
+          <div>{blog.user && blog.user.name}</div>
+        </>
+      )}
+    </div>
+  )
+}
 
 export default Blog
