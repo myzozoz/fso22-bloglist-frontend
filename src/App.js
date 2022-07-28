e212e25776e8cd9d91be9f082a513f76e52c948a
Markus Holopainen <markusp_holopainen@yahoo.com>
2022-07-28 12:37:47 +0300

    5.9

--- src/App.js
+++ src/App.js
@@ -133,9 +133,11 @@ const App = () => {
         />
       </Togglable>
 
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} handleLike={handleLike(blog)} />
-      ))}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} handleLike={handleLike(blog)} />
+        ))}
     </>
   )
 
