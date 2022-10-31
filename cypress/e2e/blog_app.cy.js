1ca1bab9613b5843586a7ee5e88b1303fe71afd0
Markus Holopainen <markusp_holopainen@yahoo.com>
2022-10-31 15:30:48 +0200

    5.18

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,11 +1,38 @@
 describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      username: 'Marcel',
+      name: 'Make',
+      password: 'marcel',
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', user)
     cy.visit('http://localhost:3000')
   })
 
   it('Login form is shown', function () {
     cy.contains('blogs')
     cy.contains('Login')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('Marcel')
+      cy.get('#password').type('marcel')
+      cy.get('#login-button').click()
+      cy.contains('Successfully logged in! Welcome Make')
+    })
+
+    it.only('fails with wrong credentials', function () {
+      cy.get('#username').type('Marcel')
+      cy.get('#password').type('wrongpassword')
+      cy.get('#login-button').click()
+      cy.get('.error')
+        .should(
+          'contain',
+          'Could not log in! Please check username and password.'
+        )
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })
