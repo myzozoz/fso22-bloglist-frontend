f17b24f1fcf56797a9d882bbcd4ed6575f16c1a0
Markus Holopainen <markusp_holopainen@yahoo.com>
2022-10-31 13:06:33 +0200

    5.15

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('Blog', () => {
-  test('renders blog in compact form', async () => {
-    const blog = {
+  //let container
+  let blog
+  let mockHandler
+  beforeEach(() => {
+    blog = {
       id: '1234asdf',
       author: 'Test Testsson',
       likes: 1111,
@@ -18,19 +22,43 @@ describe('Blog', () => {
       },
     }
 
-    render(<Blog blog={blog} handleLike={() => {}} handleDelete={() => {}} />)
-    screen.debug()
+    mockHandler = jest.fn()
+    render(
+      <Blog blog={blog} handleLike={mockHandler} handleDelete={() => {}} />
+    )
+  })
 
-    const titleElement = screen.getByText('Ten ways to stop testing', {
+  test('renders blog in compact form', () => {
+    screen.getByText('Ten ways to stop testing', {
       exact: false,
     })
-    expect(titleElement).toBeDefined()
-    const authorElement = screen.getByText('Test Testsson', { exact: false })
-    expect(authorElement).toBeDefined()
+    screen.getByText('Test Testsson', { exact: false })
 
     const urlElement = screen.queryByText('https://goingnowhere.com')
     expect(urlElement).toBeNull()
     const likeElement = screen.queryByText('1111')
     expect(likeElement).toBeNull()
+  })
+
+  test('renders details after clicking button', async () => {
+    const buttonElement = screen.getByText('view')
+    const user = userEvent.setup()
+    await user.click(buttonElement)
+
+    screen.debug()
+    screen.getByText(blog.url)
+    screen.getByText(blog.likes, { exact: false })
+  })
+
+  test('clicking like twice logs two events', async () => {
+    const viewButton = screen.getByText('view')
+    const user = userEvent.setup()
+    await user.click(viewButton)
+
+    const likeButton = screen.getByText('like')
+    await user.click(likeButton)
+    await user.click(likeButton)
+
+    expect(mockHandler.mock.calls).toHaveLength(2)
   })
 })
